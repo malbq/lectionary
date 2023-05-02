a6c6912713029462cd5d0f3152ead37dca6bb2b1
Mauro Albuquerque <maurodev@pm.me>
2023-05-01 22:23:05 -0300

    easter.week4.2

--- cli/helpers.mjs
+++ cli/helpers.mjs
@@ -2,17 +2,25 @@ export function evaluateText(liturgy) {
   let sum = 0
   let product = 1
   
-  liturgy.readings.forEach(
-    ({ text, ref }) => {
-      if (typeof text === 'object') {
-        sum += text.short.length + text.long.length + ref.length
-        product *= text.short.length * text.long.length * ref.length
-      } else {
-        sum += text.length  + ref.length
-        product *= text.length * ref.length
+  try {
+    liturgy.readings.forEach(
+      ({ text, ref, main, optional, long, short }) => {
+        if (main && optional) {
+          sum += main.text.length + optional.text.length + main.ref.length + optional.ref.length
+          product *= main.text.length * optional.text.length * main.ref.length * optional.ref.length
+        } else if (long && short) {
+          sum += long.text.length + short.text.length + long.ref.length + short.ref.length
+          product *= long.text.length * short.text.length * long.ref.length * short.ref.length
+        } else {
+          sum += text.length  + ref.length
+          product *= text.length * ref.length
+        }
       }
-    }
-  )
+    )
+  } catch (error) {
+    console.error('Error in evaluateText', liturgy)
+    throw error
+  }
 
   if (sum === 0 && product === 0) {
     return 2 // no text
